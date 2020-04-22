0822d82f1b8e743af546d795767a4c153b29ebbe
Anika <apatelcse@gmail.com>
2020-04-22 07:31:20 -0700

    added game loop

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 // declare default variables
 let flowerCount = 0;
 let clickPower = 1;
-let clickPowerPriceAmount = 5;
-let clickPowerLevelNumber = 1;
 
 // declare DOM variables
 let flowerCounter = document.getElementById('flower-counter');
 let flowerClicker = document.getElementById('flower-clicker');
-let buyClickPower = document.getElementById('buy-click-power');
-let clickPowerPrice = document.getElementById('click-power-price');
-let clickPowerLevel = document.getElementById('click-power-level');
-let clickPowerMultiple = document.getElementById('click-power-multiple');
 
 let refreshFlowerCount = function() {
     flowerCounter.innerHTML = flowerCount;
@@ -23,6 +17,22 @@ flowerClicker.addEventListener("click", function() {
     flowerCount = flowerCount + clickPower;
     refreshFlowerCount();
 })
+
+/********************************
+
+          SHED
+
+********************************/
+
+// set default values
+let clickPowerPriceAmount = 5;
+let clickPowerLevelNumber = 1;
+
+// declare DOM variables
+let buyClickPower = document.getElementById('buy-click-power');
+let clickPowerPrice = document.getElementById('click-power-price');
+let clickPowerLevel = document.getElementById('click-power-level');
+let clickPowerMultiple = document.getElementById('click-power-multiple');
 
 // click power listener
 buyClickPower.addEventListener("click", function() {
@@ -54,4 +64,63 @@ let refreshPowerClick = function() {
     clickPowerLevel.innerHTML = clickPowerLevelNumber;
     clickPowerPrice.innerHTML = clickPowerPriceAmount;
     clickPowerMultiple.innerHTML = clickPower;
+}
+
+/********************************
+
+          GNOMES
+
+********************************/
+
+// set default values
+let gnomePower = 50;
+let gnomePriceAmount = 500;
+let gnomeLevelNumber = 0;
+let gnomeAuto = false;
+
+// declare DOM variables
+let buyGnome = document.getElementById('buy-gnome');
+let gnomePrice = document.getElementById('gnome-price');
+let gnomeLevel = document.getElementById('gnome-level');
+let gnomeMultiple = document.getElementById('gnome-multiple');
+
+// buy a gnome
+buyGnome.addEventListener("click", function() {
+    // make sure there are enough flowers, subtract flowers from price
+    if (flowerCount >= gnomePriceAmount) {
+        // subtract cookies from price of item
+        flowerCount -= gnomePriceAmount;
+        refreshFlowerCount();
+
+        // upgrade power level
+        gnomeLevelNumber++;
+
+        // update price
+        gnomePriceAmount = Math.ceil(gnomePriceAmount * 1.5);
+
+        // update gnome power
+        gnomePower += 10;
+
+        // turn autoGnome on
+        autoGnome = true;
+        autoGnomeStart();
+
+        // refresh shop item
+        refreshGnome();
+    }
+})
+
+
+let autoGnomeStart = function() {
+    let gnomeInt = window.setInterval(function () {
+        flowerCount += gnomePower;
+        refreshFlowerCount();
+    }, 1000);
+}
+
+
+let refreshGnome = function() {
+    gnomeLevel.innerHTML = gnomeLevelNumber;
+    gnomePrice.innerHTML = gnomePriceAmount;
+    gnomeMultiple.innerHTML = gnomePower - 10;
 }
